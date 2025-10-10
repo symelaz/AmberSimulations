e51443bfa906c523e62c57c9ad7e3de3d0903866
Symela Lazaridi <94408242+symelaz@users.noreply.github.com>
2025-10-10 14:45:29 +0200

    Update extract_protein.tcl

--- utils/extract_protein.tcl
+++ utils/extract_protein.tcl
@@ -1,7 +1,10 @@
 #################### Inputs ####################
 # 0 --> psf file of the system to remove hydrogens and extract only the protein
 # 1 --> pdb location to be saved
+# 2 --> ligand's resname
+
+set resname [lindex $argv 2] 
 
 mol new [lindex $argv 0] type pdb
-[atomselect top "protein and noh"] writepdb [lindex $argv 1]
+[atomselect top "protein and noh and not resname $resname"] writepdb [lindex $argv 1]
 quit
