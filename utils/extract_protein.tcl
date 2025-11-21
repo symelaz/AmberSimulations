56598171c0d19eb538d87ee250d23557855e33ec
Inkenfe <64537332+Inkenfe@users.noreply.github.com>
2025-11-21 13:20:09 +0100

    Update extract_protein.tcl
    Adjust selection when no resname specified

--- utils/extract_protein.tcl
+++ utils/extract_protein.tcl
@@ -3,8 +3,15 @@
 # 1 --> pdb location to be saved
 # 2 --> ligand's resname
 
-set resname [lindex $argv 2] 
+# Check if argv has at least 3 elements
+if {[llength $argv] > 2} {
+    set resname [lindex $argv 2]
+    set seltext "protein and noh and not resname $resname"
+} else {
+    # No argv2 provided → use alternate selection, there is no ligand to unselect
+    set seltext "protein and noh"
+}
 
 mol new [lindex $argv 0] type pdb
-[atomselect top "protein and noh and not resname $resname"] writepdb [lindex $argv 1]
+[atomselect top $seltext] writepdb [lindex $argv 1]
 quit
